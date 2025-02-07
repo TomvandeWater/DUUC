7a7aa5546e2f6ecf99ef893a0871a78392c5245e
TomvandeWater <T.vandeWater-1@student.tudelft.nl>
2025-02-07 16:26:55 +0100

    Temperature is included in the output as it is required in the reynolds number to determine mu.

--- analysis_modules/ISA.py
+++ analysis_modules/ISA.py
@@ -1,4 +1,5 @@
 import constants
+import numpy as np
 
 
 def air_density_isa(altitude):
@@ -6,7 +7,7 @@ def air_density_isa(altitude):
     Calculate the air density based on the International Standard Atmosphere (ISA) model.
     :constants: are imported from constants file
     :param altitude: Altitude in meters
-    :return: Air density in kg/m³
+    :return: Air density in kg/m³ and temperature in K
     """
 
     if altitude < 11000:  # Troposphere (up to 11 km)
@@ -22,4 +23,7 @@ def air_density_isa(altitude):
                                                 (constants.R * T)))
 
     rho = P / (constants.R * T)  # Air density (kg/m³)
-    return rho
+    print("\n----- ISA calculator:")
+    print(f'Air density =', np.round(rho, 3), "kg/m3")
+    print(f'Air temperature =', np.round(T, 1), "K")
+    return np.round(rho, 3), np.round(T, 1)
