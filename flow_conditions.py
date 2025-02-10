902d8cef685e5137944500a9d4c04d0a355aa301
TomvandeWater <T.vandeWater-1@student.tudelft.nl>
2025-02-10 16:08:45 +0100

    Rounded parameters change.

--- flow_conditions.py
+++ flow_conditions.py
@@ -3,10 +3,12 @@ from analysis_modules.ISA import air_density_isa
 import numpy as np
 
 """ Flow parameters """
-rho = 0.125  # air density at sea level [kg/m^3]
 airspeed = 148  # [m/s]
 u_inf = 180  # free stream flow velocity [m/s]
 altitude = 7000  # flying altitude in [m]
+
+rho = air_density_isa(altitude)[0]
+temper = air_density_isa(altitude)[1]
 
 """ Calculated flow parameters """
 Mach = u_inf / (speed_of_sound(altitude))  # free stream mach number [-]
@@ -27,11 +29,11 @@ delta_r = 0  # rudder deflection angle [deg]
 print("\n----- Flow parameters -----")
 print("Airspeed =", u_inf, "[m/s]")
 print("Altitude =", altitude, "[m]")
-print("Mach =", np.round(Mach), '[-]')
+print("Mach =", np.round(Mach, 2), '[-]')
 print("\n ----- Angles -----")
 print("Angle of Attack:", alpha, "[deg]")
 print("Elevator deflection", delta_e, "[deg]")
 print("Rudder deflection", delta_r, "[deg]")
 print("\n----- ISA calculator -----")
-print(f'Air density =', np.round(air_density_isa(altitude)[0], 3), "[kg/m3]")
-print(f'Air temperature =', np.round(air_density_isa(altitude)[1], 1) , "[K]")
+print(f'Air density = {np.round(rho, 3)} [kg/m3]')
+print(f'Air temperature = {np.round(temper, 1)} [K]')
