75931f254f876e1f760df3828ec79ca19cbff832
TomvandeWater <T.vandeWater-1@student.tudelft.nl>
2025-02-18 14:19:56 +0100

    Fuselage parameters added.

---
+++ aircraft/fuselage.py
@@ -0,0 +1,12 @@
+import numpy as np
+import flow_conditions
+
+
+class Fuselage:
+    def __init__(self, fuselage_length: float, fuselage_diameter: float):
+        super().__init__()
+        self.fuselage_length = fuselage_length
+        self.fuselage_diameter = fuselage_diameter
+
+
+
