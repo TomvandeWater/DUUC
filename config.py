27314d6db972563145f9a903798cc3ec054d55bd
TomvandeWater <T.vandeWater-1@student.tudelft.nl>
2025-02-23 18:29:51 +0100

    Representative values for DUUC added, AR duct also added to extract chord length based on set diameter and aspect ratio.

--- config.py
+++ config.py
@@ -1,26 +1,27 @@
 """ Propulsive empennage set up"""
 duct_diameter = 3.6  # duct diameter in [m]
-duct_chord = 1.5  # duct chord in [m]
+AR_duct = 2  # aspect ratio of the duct [-]
+duct_chord = duct_diameter / AR_duct  # duct chord in [m]
 
 pylon_length = 1  # pylon length in [m]
-pylon_chord = 0.5  # pylon chord length [m]
+pylon_chord = 1  # pylon chord length [m]
 cant_angle = 30  # cant angle of the pylon wrt to the fuselage [deg]
 
-nacelle_length = 1.2  # nacelle length in [m]
-nacelle_diameter = 0.30  # nacelle diameter in [m]
+nacelle_length = 1.3  # nacelle length in [m]
+nacelle_diameter = 1.0  # nacelle diameter in [m]
 
 support_length = duct_diameter  # support length in [m]
 support_chord = 0.5  # support chord [m]
 
 control_vane_length = 0.5 * duct_diameter  # one control vane length [m]
-control_vane_chord = 0.20  # control vane chord [m]
+control_vane_chord = 0.30  # control vane chord [m]
 
 propulsor_type = "traditional"  # options are: traditional, hybrid
 n_blades = 6  # number of propeller blades [-]
 rpm = 1000  # RPM of the propulsor
 c_root = 0.2  # root chord of the propeller blade [m]
 c_tip = 0.2  # tip chord of the propeller blade [m]
-hub_diameter = 0.2  # spinner hub diameter [m]
+hub_diameter = 0.6  # spinner hub diameter [m]
 propeller_sweep = 0  # propeller sweep [deg]
 propeller_pitch = 0  # propeller pitch [deg]
 
@@ -35,3 +36,6 @@ d_exit = 1.05 * duct_diameter
 
 """ Test condition"""
 power_condition = "on"
+
+n_pax = 75  # number of passengers in the aircraft [-]
+w_pax = 80 + 22  # 80 kg weight of passenger and 22 kg of lugage [kg]
