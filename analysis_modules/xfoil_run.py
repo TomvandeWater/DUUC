21c7576bf62620a901d2e039d765f5db07426cc4
TomvandeWater <T.vandeWater-1@student.tudelft.nl>
2025-02-24 08:51:39 +0100

    Additional function added for Naca 5 series airfoil that is not included in the airfoil database.

--- analysis_modules/xfoil_run.py
+++ analysis_modules/xfoil_run.py
@@ -4,6 +4,7 @@ import numpy as np
 
 input_folder = r"C:\Users\tomva\pythonProject\DUUC\analysis_modules\input_files"
 output_folder = r"C:\Users\tomva\pythonProject\DUUC\data\Polars"
+polar_folder = r"..\data\airfoil_coordinates"
 xfoil_path = r"C:\Users\tomva\pythonProject\DUUC\analysis_modules\xfoil.exe"
 
 
@@ -45,6 +46,47 @@ def xfoil_polar(airfoil_name, alpha_i, alpha_f, alpha_step, Re, file_name, mach)
                  f"and saved in {file_name}.txt")
 
 
+def xfoil_polar_5seriesload(airfoil_name, alpha_i, alpha_f, alpha_step, Re, file_name, mach):
+    os.makedirs(input_folder, exist_ok=True)
+    input_file_path = os.path.join(input_folder, f"{file_name}_input.in")
+    coordinates_file_path = os.path.join(polar_folder, f"{airfoil_name}")
+    output_file_path = os.path.join(output_folder, f"{file_name}.txt")
+
+    if os.path.exists(output_file_path):
+        os.remove(output_file_path)
+
+    with open(input_file_path, "w") as file:
+        file.write(f"load\n")
+        file.write(f"{coordinates_file_path}.txt\n")
+        file.write(f"naca43013\n")
+        file.write("Oper\n")
+        file.write("v\n")
+        file.write(f"{Re}\n")
+        file.write("mach\n")
+        file.write(f"{mach}\n")
+        file.write("PACC\n")
+        file.write(f"{output_file_path}\n\n")
+        file.write("ASeq\n")
+        file.write(f"{alpha_i}\n")
+        file.write(f"{alpha_f}\n")
+        file.write(f"{alpha_step}\n")
+        file.write("\n\n")
+        file.write("quit\n")
+        file.close()
+
+    with open(input_file_path, "r") as f:
+        process = subprocess.Popen([xfoil_path], stdin=subprocess.PIPE,
+                                   stdout=subprocess.DEVNULL,
+                                   text=True)
+        process.communicate(input=f.read())
+
+    return print(f"\n----- Xfoil output for {file_name}\n"
+                 f"Airfoil polar has been created for {airfoil_name}, from "
+                 f"{alpha_i} to {alpha_f} in {alpha_step} steps for Reynolds "
+                 f"number {np.round(Re)} with Mach {np.round(mach, 2)} "
+                 f"and saved in {file_name}.txt")
+
+
 """ Test test if wanted"""
-"""
-xfoil_polar("naca0012", -5, 15, 1, 1.698e6, "vt0012", 0.576) """
+
+xfoil_polar_5seriesload("Naca43018", -5, 15, 1, 1.698e6, "wing43018", 0.40)
