811eeb0591d8e6cf8ff4d35f8a0d55f3d3594a24
TomvandeWater <T.vandeWater-1@student.tudelft.nl>
2025-02-27 13:21:53 +0100

    Landing Gear class added for weight estimation.

---
+++ aircraft/landing_gear.py
@@ -0,0 +1,27 @@
+import data.atr_reference as ref
+import numpy as np
+
+
+class LandingGear:
+    def __init__(self, aircraft_type: str):
+        self.aircraft_type = aircraft_type
+
+    """ ----------------------------------- Determine weight Landing Gear ---------------------------------------- """
+    def weight(self):
+        """ Landing gear mass estimated based on Torenbeek -> output in Newtons"""
+        k = 1
+        g = 9.81  # gravitational constant [m/s^2]
+        w_nose = k * (ref.a_nose + ref.b_nose * ref.MTOW ** 0.75 + ref.d_nose
+                      * ref.MTOW ** 1.5) * g
+
+        w_main = k * (ref.a_main + ref.b_main * ref.MTOW ** 0.75 + ref.d_main
+                      * ref.MTOW ** 1.5 + ref.c_main * ref.MTOW) * g
+
+        return np.round(w_nose, 0), np.round(w_main, 0), np.round(w_nose + w_main, 0)
+
+
+""" Test section"""
+"""
+if __name__ == "__main__":
+    lg = LandingGear("DUUC")
+    print(lg.weight()) """
